bac5f13d099831d7a6bbe976805404a0e66adb95
Ceratotherium simum cottoni <paulo.ossandon.ica@gmail.com>
2019-09-03 15:12:22 -0400

    Refactoring: Code Refactoring

--- src/main/java/com/core/customer/controller/CustomerController.java
+++ src/main/java/com/core/customer/controller/CustomerController.java
@@ -7,8 +7,12 @@ import com.core.customer.exception.CustomerNotFoundException;
 import com.core.customer.interfaz.CustomerRepository;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.hateoas.Resources;
 import org.springframework.hateoas.Resource;
@@ -19,80 +23,41 @@ class CustomerController{
     @Autowired(required=true)
     private CustomerRepository repository;
 
+    /**
+     * curl http://localhost:8080/customers/5d4ef92b66dca14d45fdffbd
+     */
     @GetMapping("/customers/{id}")
-    Resource<Customer> one(@PathVariable String id) {
+    Resource<Customer> getCustomerById(@PathVariable String id) {
         Customer customer = repository.findById(id)
         .orElseThrow(() -> new CustomerNotFoundException(id));
 
         return new Resource<>(customer,
-        linkTo(methodOn(CustomerController.class).one(id)).withSelfRel(),
-        linkTo(methodOn(CustomerController.class).all()).withRel("customers"));
+        linkTo(methodOn(CustomerController.class).getCustomerById(id)).withSelfRel(),
+        linkTo(methodOn(CustomerController.class).getCustomer()).withRel("customers"));
     }
 
+    /**
+     * curl http://localhost:8080/customers
+     */
     @GetMapping("/customers")
-    Resources<Resource<Customer>> all() {
+    Resources<Resource<Customer>> getCustomer() {
         List<Resource<Customer>> customers = repository.findAll().stream()
         .map(employee -> new Resource<>(employee,
-            linkTo(methodOn(CustomerController.class).one(employee.getId())).withSelfRel(),
-            linkTo(methodOn(CustomerController.class).all()).withRel("customers")))
+            linkTo(methodOn(CustomerController.class).getCustomerById(employee.getId())).withSelfRel(),
+            linkTo(methodOn(CustomerController.class).getCustomer()).withRel("customers")))
         .collect(Collectors.toList());
 
         return new Resources<>(customers,
-        linkTo(methodOn(CustomerController.class).all()).withSelfRel());
+        linkTo(methodOn(CustomerController.class).getCustomer()).withSelfRel());
     }
 
-    /** Customer getCustomerById(@PathVariable String id){
-        return repository.findById(id)
-			.orElseThrow(() -> new CustomerNotFoundException(id));
-    }**/
-    
-    /**
-     * curl localhost:8080/customers/5d4ef92b66dca14d45fdffbd
-     * 
-     * @param string
-     * @return
-     */
-    /*@GetMapping("/customers/{id}")
-    Resource<Customer> one(@PathVariable ObjectId id) {
-        Customer customer = repository.findById(id)
-        .orElseThrow(() -> new CustomerNotFoundException(id));
-
-        return new Resource<>(customer,
-        linkTo(methodOn(CustomerController.class).one(id)).withSelfRel(),
-        linkTo(methodOn(CustomerController.class).all()).withRel("customers"));
-    }
-
-    @GetMapping("/customers")
-    Resources<Resource<Customer>> all() {
-        List<Object> customers = repository.findAll().stream()
-        .map(customer -> new Resource<>(customer,
-            linkTo(methodOn(CustomerController.class).one(customer.getId())).withSelfRel(),
-            linkTo(methodOn(CustomerController.class).all()).withRel("customers")))
-        .collect(Collectors.toList());
-
-        return new Resources<>(customers,
-        linkTo(methodOn(CustomerController.class).all()).withSelfRel());
-    }**/
-
-    /*@GetMapping("/customers")
-    Resources<Resource<Customer>> getCustomer(){
-        List<Object> customers = repository.findAll().stream()
-            .map(customer -> new Resource<>(customer,
-            linkTo(methodOn(CustomerController.class).one(customer.getId())).withSelRefl(),
-            linkTo(methodOn(CustomerController.class).getCustomer()).withRel("customer")))
-            .collect(Collectors.toList());
-            
-            return new Resources<>(customers,
-                linkTo(methodOn(CustomerController.class).getCustomer()).withSelRefl());
-    }**/
-
-    /**
+     /**
      * curl -X POST localhost:8080/customers -H 'Content-type:application/json' -d '{"firstName": "Roberto", "secondName": "Carlos"}'
      */
-    /*@PostMapping("/customers")
+    @PostMapping("/customers")
     Customer createCustomer(@RequestBody Customer newCustomer){
         return repository.save(newCustomer);
-    }**/
+    }
 
     /**
      * curl -X PUT localhost:8080/customers/5d4f5e6f36f6a3049689e2fa -H 'Content-type:application/json' -d '{"firstName": "MR", "secondName": "ROBOT"}'
@@ -100,8 +65,8 @@ class CustomerController{
      * @param id
      * @return
      */
-    /*@PutMapping("/customers/{id}")
-    Customer updateCustomerById(@RequestBody Customer newCustomer, @PathVariable ObjectId id){
+    @PutMapping("/customers/{id}")
+    Customer updateCustomerById(@RequestBody Customer newCustomer, @PathVariable String id){
         return repository.findById(id)
             .map(customer -> {
                 customer.setFirstName(newCustomer.getFirstName());  
@@ -112,13 +77,14 @@ class CustomerController{
                 newCustomer.setId(id);
                 return repository.save(newCustomer);
             });
-    }***/
-
+    }
+    
     /**
      * curl -X DELETE localhost:8080/customers/5d4efd19f5e5480b32bd1339
      */
-    /** @DeleteMapping("/customers/{id}")
-    void deleteCustomerById(@PathVariable ObjectId id){
+    @DeleteMapping("/customers/{id}")
+    void deleteCustomerById(@PathVariable String id){
         repository.deleteById(id);
-    }**/
+    }
+    
 }
